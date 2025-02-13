ce2590c803014a7a123fbff2d00550e85048f5e5
Jean Kahlil Ribeiro <jeankahlilribeiro@gmail.com>
2025-02-12 23:26:58 -0300

    Função Adicionar amigos + Função StringVazia

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigo = [];
+
+function adicionarAmigo(){
+
+    let adicionar = document.querySelector('input').value;
+
+    if (adicionar == ''){
+        alert('Por Favor, insira um nome.');
+    }else{
+        amigo.push(adicionar);
+        console.log(amigo);        
+    }
+    stringVazia()
+}
+
+
+
+function stringVazia(){
+    adicionar = document.querySelector('input');
+    adicionar.value = '';
+}

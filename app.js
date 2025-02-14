cc768992070a6fb9073a8808c0f973da2584a42b
Jean Kahlil Ribeiro <jeankahlilribeiro@gmail.com>
2025-02-13 22:35:11 -0300

    Sorteio de amigos + Lista de amigos

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+
 let amigo = [];
 
 function adicionarAmigo(){
 
     let adicionar = document.querySelector('input').value;
 
-    if (adicionar == ''){
+    if (adicionar.trim() == ''){
         alert('Por Favor, insira um nome.');
-    }else{
+    }else{        
         amigo.push(adicionar);
-        console.log(amigo);        
+        listaDeAmigos();                
     }
-    stringVazia()
+
+    stringVazia();
+
+    return amigo;
+    
+}
+
+
+function sortearAmigo(){  
+    let tamanhoAmigo = amigo.length;
+    sorteioNumero = parseInt(Math.random() * tamanhoAmigo );
+    let amigoSorteado = amigo[sorteioNumero];
+    console.log(sorteioNumero, amigoSorteado);    
 }
 
 
 
+
 function stringVazia(){
-    adicionar = document.querySelector('input');
-    adicionar.value = '';
+    adicionar = document.querySelector('input').value = '';
+}
+
+
+function listaDeAmigos() {
+    const input = document.getElementById('amigo');
+    const nome = input.value;    
+    const lista = document.getElementById('listaAmigos');
+    const novoItem = document.createElement('li');
+    novoItem.textContent = nome;
+    lista.appendChild(novoItem);      
 }
